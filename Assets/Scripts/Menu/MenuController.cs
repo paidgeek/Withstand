d2a1531da3ee368a8a44ae1d88e8c32f6e43e735
paidgeek <tin.rabzelj@gmail.com>
2016-12-10 07:12:28 +0100

    menu

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : Singleton<MenuController>
+{
+  public void OnTwitterClick()
+  {
+    Application.OpenURL("https://twitter.com/paidgeek");
+  }
+
+  public void OnOptionsClick() {}
+
+  public void OnPlayClick()
+  {
+    SceneManager.LoadScene("Game");
+  }
+}
---
+++ Assets/Scripts/Menu/OptionsController.cs
@@ -0,0 +1,3 @@
+﻿public class OptionsController : Singleton<OptionsController>
+{
+}
