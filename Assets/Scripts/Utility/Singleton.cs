fc1eecbc6b9d2188d6cd4b45bc2cb70e3165a6c8
paidgeek <tin.rabzelj@gmail.com>
2016-12-10 03:33:25 +0100

    base

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+  private static T s_Instance;
+  private static readonly object s_Lock = new object();
+
+  public static T instance
+  {
+    get
+    {
+      lock (s_Lock) {
+        if (s_Instance == null) {
+          s_Instance = FindObjectOfType<T>();
+        }
+
+        return s_Instance;
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Utility
+{
+  public static bool RandomBool()
+  {
+    return Random.Range(0, 2) == 0;
+  }
+
+  public static int RandomSign()
+  {
+    return Random.Range(0, 2) == 0 ? -1 : 1;
+  }
+
+  public static void SetLayerRecursively(this GameObject gameObject, int layer)
+  {
+    gameObject.layer = layer;
+    var t = gameObject.transform;
+
+    for (var i = 0; i < t.childCount; i++) {
+      t.GetChild(i).gameObject.SetLayerRecursively(layer);
+    }
+  }
+}
